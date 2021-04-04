3e8487abe8d09c9463754787f77bc46ed7a360b4
KimHyunsoo <tomy9729@naver.com>
2021-04-04 16:54:33 +0900

    Delete 1. 장고/askcompany directory

--- 1. 장고/askcompany/blog1/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Post(models.Model): #어떤 post를 저장
-    title = models.CharField(max_length=100) #제목은 최대 100자
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)#저장시간
-    updated_at = models.DateTimeField(auto_now=True)#업데이트 시간
-#위 구조로 데이터베이스에 저장되게 됩니다.
