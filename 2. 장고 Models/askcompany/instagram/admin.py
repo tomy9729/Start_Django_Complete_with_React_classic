e0d9e386efee8f8af34fdd5162d8e3cb80a418c5
KimHyunsoo <tomy9729@naver.com>
2021-04-09 14:41:19 +0900

    Update admin.py

--- 2. 장고 Models/askcompany/instagram/admin.py
+++ 2. 장고 Models/askcompany/instagram/admin.py
@@ -1,3 +1,18 @@
+#instagram/admin.py
 from django.contrib import admin
+from .models import Post
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+@admin.register(Post) 
+class PostAdmin(admin.ModelAdmin) : 
+    list_display = ['pk','message','photo_tag','message_length','created_at','is_public','updated_at'] #primary key(id)
+    list_display_links = ['message'] # 여러개 넣을 수 있다.
+    list_filter = ['created_at', 'is_public']
+    search_fields = ['message'] # 메세지를 통해 검색
+
+    def photo_tag(self, post) : 
+        if post.photo : 
+            return mark_safe(f'<img src = "{post.photo.url}" style="width:50px"/>')
+        return None
+    def message_length(self, post):
+        return f"{len(post.message)}글자"
